23021ac86f194d1fd040e60bd162b00f1d556e75
DrokaBit <pedroantoniopmartinello16@gmail.com>
2021-09-06 17:11:48 -0300

    Create exercicio7.py

---
+++ exercicio7.py
@@ -0,0 +1,9 @@
+listaFilme = [Senhor_dos_anéis,Harry_potter]
+listaJogos = [God_of_War,Counter_Strike_Source]
+listaLivros = [O_Diario_de_anne_frank,Jojos_Bizarre_Adventures]
+listaEsportes = [Handebol,basquete]
+
+listaMÃE = [listaFilme,listaEsportes,listaJogos,listaLivros]
+print(listaMÃE(listaLivros(0)))
+del listaEsportes[0]
+
