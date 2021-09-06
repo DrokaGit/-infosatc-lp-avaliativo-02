b0e68cf69c367475528499f1d1acfaed04793d9d
DrokaBit <pedroantoniopmartinello16@gmail.com>
2021-09-06 17:11:34 -0300

    Create exercicio3.py

---
+++ exercicio3.py
@@ -0,0 +1,9 @@
+nume1 = int(input("Digite um numero"))
+nume2 = int(input("Digite um numero"))
+nume3 = int(input("Digite um numero"))
+nume4 = int(input("Digite um numero"))
+nume5 = int(input("Digite um numero"))
+
+table = [nume1,nume2,nume3,nume4,nume5]
+tableM = (float((nume1 + nume2 + nume3 + nume4 + nume5)))
+print(float(tableM))
