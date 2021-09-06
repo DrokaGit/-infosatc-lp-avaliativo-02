e0dd34fef9e0ceb8a962335f191d59fcf6861af1
DrokaBit <pedroantoniopmartinello16@gmail.com>
2021-09-06 17:11:53 -0300

    Create exercicio9.py

---
+++ exercicio9.py
@@ -0,0 +1,7 @@
+nume1 = (input("Digite um numero"))
+nume2 = (input("Digite um numero"))
+nume3 = (input("Digite um numero"))
+nume4 = (input("Digite um numero"))
+nume5 = (input("Digite um numero"))
+lista_nova =[nume1,nume2,nume3,nume4,nume5]
+print("Sua lista ordenada numérica: ", sorted(lista_nova))
