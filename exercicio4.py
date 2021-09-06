d4064ce859fdf362880931af2f018b873899d897
DrokaBit <pedroantoniopmartinello16@gmail.com>
2021-09-06 17:11:39 -0300

    Create exercicio4.py

---
+++ exercicio4.py
@@ -0,0 +1,10 @@
+nume1 = (input("Digite um numero"))
+nume2 = (input("Digite um numero"))
+nume3 = (input("Digite um numero"))
+nume4 = (input("Digite um numero"))
+nume5 = (input("Digite um numero"))
+
+lista = (nume1,nume2,nume3,nume4,nume5)
+
+lista = lista.copy()
+print(lista_copia)
